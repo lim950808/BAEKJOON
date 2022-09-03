d96d6cfe92e2e7ccc39e72784aa7ff1a8c5f4985
lim950808 <lim080895@gmail.com>
2022-09-03 22:45:26 +0900

    별 찍기 - 1_2438

--- BAEKJOON/src/Implementation/star1.java
+++ BAEKJOON/src/Implementation/star1.java
@@ -13,6 +13,8 @@ public class star1 {
 			for(int j=0; j < i+1; j++) {
 				System.out.print("*");
 			}
+			//첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개 출력
+			//첫째 줄부터 N번째 줄까지 차례대로 별을 출력
 			System.out.println("");
 		}
 		

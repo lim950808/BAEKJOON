b867f780166a141302295c27da67166695e4ba15
lim950808 <lim080895@gmail.com>
2022-09-03 22:39:33 +0900

    별 찍기 - 1_2438

---
+++ BAEKJOON/src/Implementation/star1.java
@@ -0,0 +1,21 @@
+package Implementation;
+
+import java.util.Scanner;
+//º° Âï±â - 1
+public class star1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j < i+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		
+	}
+
+}

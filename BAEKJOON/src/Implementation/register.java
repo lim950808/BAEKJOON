953ce2381c59e56f2187c454f21d64f869ed8876
lim950808 <lim080895@gmail.com>
2022-09-11 16:36:39 +0900

    등록_7287

---
+++ BAEKJOON/src/Implementation/register.java
@@ -0,0 +1,12 @@
+package Implementation;
+//등록
+public class register {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(42); //첫 줄에 자신이 맞은 문제의 수,
+		System.out.println("lim950808"); //둘째 줄에 아이디를 출력
+		
+	}
+	
+}

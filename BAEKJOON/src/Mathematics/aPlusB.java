785b98e949d41af69d05779001ec808175dae3f1
lim950808 <lim080895@gmail.com>
2022-09-14 00:43:25 +0900

    A+B_1000

--- BAEKJOON/src/Mathematics/aPlusB.java
+++ BAEKJOON/src/Mathematics/aPlusB.java
@@ -9,8 +9,8 @@ public class aPlusB {
 	public static void main(String[] args) throws IOException {
 		     
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		String[] str = br.readLine().split(" ");
+		
 		int a = Integer.parseInt(str[0]);
 		int b = Integer.parseInt(str[1]);
 		

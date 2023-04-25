d1fa259017b2454353f441c083bca0b017272b20
Jae Eok Lim <lim080895@gmail.com>
2023-04-25 21:45:33 +0900

    P2743_단어길이재기

---
+++ BAEKJOON/src/Implementation/P2743_단어길이재기.java
@@ -0,0 +1,11 @@
+package Implementation;
+
+import java.io.*;
+
+public class P2743_단어길이재기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine(); // pulljima
+        System.out.print(str.length()); // 8
+    }
+}

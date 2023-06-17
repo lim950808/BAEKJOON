01c1e720e0f64fcb4c5c5b76eb46c9eca28b406d
Jae Eok Lim <lim080895@gmail.com>
2023-06-17 20:49:31 +0900

    P2941_크로아티아알파벳

---
+++ BAEKJOON/src/Implementation/P2941_크로아티아알파벳.java
@@ -0,0 +1,43 @@
+package Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2941_크로아티아알파벳 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int len = str.length();
+        int cnt = 0;
+
+        for (int i = 0; i < len; i++) {
+            char c = str.charAt(i);
+            if (c == 'c' && i < len - 1) {
+                if (str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-') {
+                    i++;
+                }
+            } else if (c == 'd' && i < len - 1) {
+                if (str.charAt(i + 1) == '-') {
+                    i++;
+                } else if (str.charAt(i + 1) == 'z' && i < len - 2) {
+                    if(str.charAt(i + 2) == '=') {
+                        i += 2;
+                    }
+                }
+            } else if ((c == 'l' || c == 'n') && i < len - 1) {
+                if(str.charAt(i + 1) == 'j') {
+                    i++;
+                }
+            } else if ((c == 's' || c == 'z') && i < len - 1) {
+                if (str.charAt(i + 1) == '=') {
+                    i++;
+                }
+            }
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}

6dcd02f33ea389ceb4578027f027e61d0f1cdae6
Jae Eok Lim <lim080895@gmail.com>
2023-08-17 23:56:52 +0900

    P10172_개

---
+++ BAEKJOON/src/Implementation/P10172_개.java
@@ -0,0 +1,14 @@
+package Implementation;
+
+public class P10172_개 {
+    public static void main(String[] args){
+        StringBuffer sb = new StringBuffer();
+        sb.append("|\\_/|\n");
+        sb.append("|q p|   /}\n");
+        sb.append("( 0 )\"\"\"\\\n");
+        sb.append("|\"^\"`    |\n");
+        sb.append("||_/=\\\\__|\n");
+
+        System.out.println(sb);
+    }
+}

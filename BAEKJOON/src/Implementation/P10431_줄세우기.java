01d739662e4c4a9addfdaedac5755f13555ee36a
Jae Eok Lim <lim080895@gmail.com>
2023-10-10 23:01:29 +0900

    P10431_줄세우기

---
+++ BAEKJOON/src/Implementation/P10431_줄세우기.java
@@ -0,0 +1,39 @@
+package Implementation;
+
+import java.util.Scanner;
+
+public class P10431_줄세우기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("테스트 케이스의 수 P 입력 : ");
+        int P = sc.nextInt();
+
+        System.out.println("테스트 케이스 번호 T와 20개의 양의 정수를 공백으로 구분되어 입력 : ");
+        while (P-- > 0) {
+            int T = sc.nextInt();
+            int[] height = new int[20];
+
+            for (int i = 0; i < 20; i++)
+                height[i] = sc.nextInt();
+
+            int cnt = 0;
+
+            for (int i = 0; i < 20; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (height[j] > height[i]) {
+                        int h = height[i];
+                        for (int k = i; k > j; k--) {
+                            height[k] = height[k - 1];
+                            cnt++;
+                        }
+                        height[j] = h;
+                        break;
+                    }
+                }
+            }
+
+            System.out.println(T + " " + cnt);
+        }
+    }
+}
